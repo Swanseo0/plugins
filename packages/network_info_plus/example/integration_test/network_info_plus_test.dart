1c1e25f2ff1250f67d8239c502ae19abd24f0323
swan.seo <swan.seo@samsung.com>
2023-08-25 18:41:24 +1100

    Update integration_test

--- packages/network_info_plus/example/integration_test/network_info_plus_test.dart
+++ packages/network_info_plus/example/integration_test/network_info_plus_test.dart
@@ -1,7 +1,6 @@
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:network_info_plus/network_info_plus.dart';
@@ -9,21 +8,19 @@ import 'package:network_info_plus/network_info_plus.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('NetworkInfo test driver', () {
-    late NetworkInfo _networkInfo;
+  late NetworkInfo networkInfo;
 
-    setUpAll(() async {
-      _networkInfo = NetworkInfo();
-    });
+  setUp(() async {
+    networkInfo = NetworkInfo();
+  });
 
-    testWidgets('test non-null network value', (WidgetTester tester) async {
-      expect(_networkInfo.getWifiName(), isNotNull);
-      expect(_networkInfo.getWifiBSSID(), isNotNull);
-      expect(_networkInfo.getWifiIP(), isNotNull);
-      expect(_networkInfo.getWifiIPv6(), isNotNull);
-      expect(_networkInfo.getWifiSubmask(), isNotNull);
-      expect(_networkInfo.getWifiGatewayIP(), isNotNull);
-      expect(_networkInfo.getWifiBroadcast(), isNotNull);
-    });
+  testWidgets('test non-null network value', (WidgetTester tester) async {
+    expect(networkInfo.getWifiName(), isNotNull);
+    expect(networkInfo.getWifiBSSID(), isNotNull);
+    expect(networkInfo.getWifiIP(), isNotNull);
+    expect(networkInfo.getWifiIPv6(), isNotNull);
+    expect(networkInfo.getWifiSubmask(), isNotNull);
+    expect(networkInfo.getWifiGatewayIP(), isNotNull);
+    expect(networkInfo.getWifiBroadcast(), isNotNull);
   });
 }
